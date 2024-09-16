c5744ad8ac1f5d712b04b1e4fc4a7ba1d4a3cf29
An Pham <anpham2112003>
2024-09-16 22:02:11 +0700

    Add project files.

---
+++ Infrastructure/DB/SQLDbContext/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DB.SQLDbContext
+{
+    public class ApplicationDBContext:DbContext
+    {
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+   
+}
